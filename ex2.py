89410e760124acec3e9db60d61afff5ee7d1ee78
Davi Sobreira de Farias <69163509+davisobreira@users.noreply.github.com>
2023-08-04 07:16:47 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,9 @@
+import datetime
+
+print ('Olá!')
+nome = input ('Informe o seu nome: ')
+
+d = input('Informe o dia que você nasceu: ')
+m = input ('Mês: ')
+a = input ('Ano: ')
+print (nome, 'nasceu em ', d, '-', m, '-', a)
